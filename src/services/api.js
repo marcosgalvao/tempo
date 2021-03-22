9e1e7a42ba276868a06f1bb808592da32d9118ce
marcosgalvao <65200174+marcosgalvao@users.noreply.github.com>
2021-03-22 18:23:49 -0400

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-// https://api.hgbrasil.com/weather?key=fbd5b149&lat=-23.682&lon=-46.875
-
-export const key = '879ef901';
+export const key = 'SUA_CHAVE';
 
 const api = axios.create({
     baseURL: 'https://api.hgbrasil.com'
